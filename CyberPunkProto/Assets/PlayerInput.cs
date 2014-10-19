ad8167437500c3e267321d9d6c9013160c68af94
Max <audiblescholar@gmail.com>
2014-10-19 13:38:00 -0600

    added unity proto
    contains at this time: basic move/look

---
+++ CyberPunkProto/Assets/PlayerInput.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour 
+{
+	public Transform playerObject;
+	PlayerMovement playerMovement;
+	bool canMovePlayer;
+
+	void OnEnable()
+	{
+		playerMovement = playerObject.GetComponent<PlayerMovement>();
+	}
+
+	void Start()
+	{
+		canMovePlayer = true;
+		StartCoroutine("InputCheck");
+	}
+
+	IEnumerator InputCheck()
+	{
+		while (canMovePlayer)
+		{
+			CheckRotation();
+
+			CheckMovement();
+
+			yield return 0;
+		}
+	}
+
+	void CheckMovement()
+	{
+		if (Input.anyKey)
+		{
+			Vector3 direction = Vector3.zero;
+
+			if (Input.GetAxis("Horizontal") != 0)
+			{
+				direction.x = Input.GetAxisRaw ("Horizontal");
+			}
+			if (Input.GetAxis("Vertical") != 0)
+			{
+				direction.z = Input.GetAxisRaw ("Vertical");
+			}
+			playerMovement.MovePlayer(direction.normalized); //Make movement camera relative?
+		}
+	}
+
+	void CheckRotation()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit))
+		{
+			if (Vector3.Distance(playerObject.position, hit.point) > 1.0f)
+			{
+				playerMovement.RotatePlayer(hit.point);
+			}
+		}
+	}
+}
---
+++ CyberPunkProto/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	bool canMove;
+
+	void OnEnable()
+	{
+		canMove = true;
+	}
+
+	public void MovePlayer(Vector3 direction)
+	{
+		if (canMove)
+		{
+			Vector3 movement = direction * 5f * Time.deltaTime; //The '5f' should be replaced with a function that accelerates at start of movement and decelerates at end.
+			rigidbody.MovePosition(rigidbody.position + movement);
+		}
+	}
+
+	public void RotatePlayer(Vector3 direction)
+	{
+		if (canMove)
+		{
+			Quaternion rotation = Quaternion.Slerp(rigidbody.rotation,Quaternion.LookRotation(direction - rigidbody.position), 5f * Time.deltaTime);
+			rigidbody.MoveRotation(rotation);
+		}
+	}
+}
